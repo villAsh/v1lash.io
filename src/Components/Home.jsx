9cb3cab456f59f39a881ac6a2ab144abd9c75bbc
Vilas Chavhan <chavhanvilas02@gmail.com>
2023-03-26 12:49:51 +0530

    created links

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,15 +8,21 @@ export default function Home() {
     const links = [
         {
             id : 1,
-            icon : <AiFillGithub />
+            name : 'github',
+            icon : <AiFillGithub />,
+            link : 'https://github.com/villAsh'
         },
         {
             id: 2,
-            icon : <AiOutlineMail /> 
+            name : 'mail',
+            icon : <AiOutlineMail />,
+            link : 'chavhanvilas02@gmail.com'
         },
         {
             id : 3,
-            icon : <AiFillLinkedin />
+            name : 'linkedIn',
+            icon : <AiFillLinkedin />,
+            link : 'https://www.linkedin.com/in/vilas-chauvhan-4609391aa'
         },
     ]
     return (
@@ -36,8 +42,17 @@ export default function Home() {
                 A self Taught Frontend Developer Who loves building <span className="text-light-primary">Web Apps</span> and <span className="text-light-primary">Solving Problems</span>.<br />
                 During my College journey i have Learnt various <span className="text-light-primary">Tools</span> and <span className="text-light-primary">Technologies</span>.
             </p>
-            <ul>
-                {links}
+            <ul className="flex items-center md:space-x-5 my-5  text-3xl">
+                {links.map((item) => <li className="relative group hover:cursor-pointer hover:text-light-primary" key={item.id}>
+                        {
+                        item.name === 'mail' ? (
+                            <a href="mailto:chavhanvilas02@gmail.com" className="transition-all">{item.icon}</a>
+                        ) : (<a href={`${item.link}`} className="transition-all" target="_blank">{item.icon}</a>)
+                        }
+                        {/* <span className="transition-all">{item.icon}</span> */}
+                        <span className="absolute -bottom-2 left-0 w-0 h-[3px] bg-light-primary transition-all group-hover:w-full"></span>
+                    </li>
+                    )}
             </ul>
         </div>
     );
